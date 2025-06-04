0713ee4338e04f5aa9db4cab4df23869fc705c7d
acentelles <centelles.alberto@gmail.com>
2025-06-04 11:29:51 +0100

    Change step size to 120

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,7 @@ pub fn run_fib(
     callback.call1(&this, &JsValue::from_str("Start"))?;
 
     //let step_size = StepSize::new(step_size as usize);
-    let step_size = StepSize::new(20);
+    let step_size = StepSize::new(100);
     callback.call1(&this, &JsValue::from_str("Start pp"))?;
     let pp_start = now();
     let pp = WasmSNARK::<E, S1, S2>::setup(step_size);
